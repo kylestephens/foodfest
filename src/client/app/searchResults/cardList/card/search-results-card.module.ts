d42432bdd26640993f8f3ca6f52571932bbd5ebc
Ana Slipcevic <slipcevic.ana@gmail.com>
2017-03-23 17:49:50 +0000

    Search results module

--- src/client/app/searchResults/cardList/card/search-results-card.module.ts
+++ src/client/app/searchResults/cardList/card/search-results-card.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } 					        from '@angular/core';
+import { CommonModule }               from '@angular/common';
 import { SearchResultsCardComponent } from './search-results-card.component';
 
 @NgModule({
-	exports: [SearchResultsCardComponent],
-  	declarations: [SearchResultsCardComponent]
+  imports: [CommonModule],
+  exports: [SearchResultsCardComponent],
+  declarations: [SearchResultsCardComponent]
 })
 
 export class SearchResultsCardModule { }
