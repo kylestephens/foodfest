1209d00f420b047cdb621cefb9f0727400c6ed3d
Ana Slipcevic <slipcevic.ana@gmail.com>
2017-03-23 22:00:13 +0000

    Pagination component

--- src/client/app/searchResults/cardList/search-results-card-list.module.ts
+++ src/client/app/searchResults/cardList/search-results-card-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } 						          from '@angular/core';
+import { Ng2PaginationModule }            from 'ng2-pagination';
 import { CommonModule } 					        from '@angular/common';
 import { SearchResultsCardListComponent } from './search-results-card-list.component';
 import { SearchResultsCardModule } 			  from './card/search-results-card.module';
 
 @NgModule({
-  imports: [CommonModule, SearchResultsCardModule],
+  imports: [Ng2PaginationModule, CommonModule, SearchResultsCardModule],
   exports: [SearchResultsCardListComponent],
   declarations: [SearchResultsCardListComponent]
 })
--- src/client/app/system-config.ts
+++ src/client/app/system-config.ts
@@ -4,9 +4,11 @@ System.config(JSON.parse('<%= SYSTEM_CONFIG_DEV %>'));
 
 System.config({
   map: {
-    'ng2-webstorage': 'ng2-webstorage'
+    'ng2-webstorage': 'ng2-webstorage',
+    'ng2-pagination': 'ng2-pagination'
   },
   packages: {
-    'ng2-webstorage': {main: 'bundles/core.umd.js', defaultExtension: 'js'}
+    'ng2-webstorage': {main: 'bundles/core.umd.js', defaultExtension: 'js'},
+    'ng2-pagination': {main: 'index.js', defaultExtension: 'js'}
   }
 });
