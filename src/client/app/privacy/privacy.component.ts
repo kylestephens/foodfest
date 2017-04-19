37a541d64e28185c3d4b0d9e98ff7601e8836e2f
Kyle Stephens <kylestephens@live.ie>
2017-04-19 15:54:57 +0100

    Adding privacy terms

---
+++ src/client/app/privacy/privacy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded PrivacyComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'ak-privacy',
+  templateUrl: 'privacy.component.html',
+  styleUrls: ['privacy.component.css']
+})
+export class PrivacyComponent { }
