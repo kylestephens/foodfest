3f9a3f31c9ff58f7817d58a93ded23547b070bae
Ana Slipcevic <slipcevic.ana@gmail.com>
2017-04-24 01:10:36 +0100

    Remove ServiceModule import

--- src/client/app/vendor/vendor.module.ts
+++ src/client/app/vendor/vendor.module.ts
@@ -2,15 +2,13 @@ import { NgModule }             from '@angular/core';
 import { VendorComponent }      from './vendor.component';
 import { VendorRoutingModule }  from './vendor-routing.module';
 import { SharedModule }         from '../shared/shared.module';
-import { ServicesModule }       from '../services/services.module';
 import { AgmCoreModule }        from 'angular2-google-maps/core';
 
 @NgModule({
   imports: [
     AgmCoreModule,
     VendorRoutingModule,
-    SharedModule,
-    ServicesModule
+    SharedModule
   ],
   declarations: [VendorComponent],
   exports: [ VendorComponent],
