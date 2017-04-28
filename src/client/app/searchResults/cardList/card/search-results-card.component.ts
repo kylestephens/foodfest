4bed0b2f7e4ac6f4bc74c316aeece05523e753d8
Kyle Stephens <kylestephens@live.ie>
2017-04-29 00:44:40 +0100

    Adding favouriting

--- src/client/app/searchResults/cardList/card/search-results-card.component.ts
+++ src/client/app/searchResults/cardList/card/search-results-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } 	            from '@angular/core';
+import { CONSTANT }                       from '../../../core/constant';
 import { Vendor } 			                  from '../../../shared/model/vendor';
+import { AccountService }                 from '../../../services/account.service';
+import { MessagingService }               from '../../../services/messaging.service';
+import { RestService }                    from '../../../services/rest.service';
 import { SettingsService }                from '../../../services/settings.service';
 
 @Component({
@@ -15,7 +19,12 @@ export class SearchResultsCardComponent {
 
   private serverUrl: string = this.settingsService.getServerBaseUrl() + '/';
 
-  constructor(private settingsService: SettingsService) {};
+  constructor(
+    private accountService: AccountService,
+    private messagingService: MessagingService,
+    private restService: RestService,
+    private settingsService: SettingsService
+  ) {};
 
   //TODO: needed in case of cross browsing, not needed for now. Maybe will be later when deployed on server.
   // ngOnInit() {
@@ -25,7 +34,26 @@ export class SearchResultsCardComponent {
 
   //TODO: a placeholder for handling reacion on liked/unliked event:<EmitEvent>
   updateFavourites(event: any) {
-    //event.elementId, event.isLiked
+    let params = {
+      user_id: this.accountService.getUser().id,
+      vendor_id: event.elementId,
+      add: event.isLiked
+    };
+    this.restService.post(
+      this.settingsService.getServerBaseUrl() + '/users/favourite',
+      params,
+      this.accountService.getUser().akAccessToken
+    ).then(function(response: any) {
+      // do nothing
+      // be happy :)
+    }, (reason: any) => {
+      this.messagingService.show(
+        'global',
+        CONSTANT.MESSAGING.ERROR,
+        reason.statusText ? reason.statusText : 'An unexpected error has occurred',
+        true
+      );
+    });
   }
 
   heartClicked(event: any) {
