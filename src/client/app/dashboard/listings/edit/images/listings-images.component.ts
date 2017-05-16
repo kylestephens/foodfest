f52886a5399b72532eb0c7077defaad518c5c439
Kyle Stephens <kylestephens@live.ie>
2017-05-16 01:50:22 +0100

    Adding image edits template

--- src/client/app/dashboard/listings/edit/images/listings-images.component.ts
+++ src/client/app/dashboard/listings/edit/images/listings-images.component.ts
@@ -1,9 +1,12 @@
 import {
   Component,
   Input,
+  OnInit,
   ViewEncapsulation
 }                                        from '@angular/core';
+
 import { Vendor }                        from '../../../../shared/model/vendor';
+import { SettingsService }               from '../../../../services/settings.service';
 
 /**
  * This class represents the lazy loaded listing edits component
@@ -16,9 +19,58 @@ import { Vendor }                        from '../../../../shared/model/vendor';
   encapsulation: ViewEncapsulation.None
 })
 
-export class ListingsImagesComponent {
+export class ListingsImagesComponent implements OnInit {
 
   @Input()
   vendor: Vendor;
+
+  public editingVendor: Vendor;
+
+  public businessLogo: Object = null;
+  public coverImage: Object = null;
+  public additionalImages: Array<Object> = [];
+
+  private serverUrl: string = this.settingsService.getServerBaseUrl() + '/';
+
+  constructor(
+    private settingsService: SettingsService
+  ) {};
+
+  ngOnInit() {
+    debugger;
+    this.editingVendor = this.vendor;
+  };
+
+  /**
+   * Convert logo image file into base64 encoded file
+   */
+  public onChangeLogo = function(fileInput: any) {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      var me = this,
+        reader = new FileReader();
+
+      reader.onload = function (e : any) {
+        me.businessLogo = e.target.result;
+      }
+
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  };
+
+  /**
+   * Convert cover image file into base64 encoded file
+   */
+  public onChangeCover = function(fileInput: any) {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      var me = this,
+        reader = new FileReader();
+
+      reader.onload = function (e : any) {
+        me.coverImage = e.target.result;
+      }
+
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  };
 
 }
