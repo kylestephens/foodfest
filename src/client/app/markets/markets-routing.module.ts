b9904935d5bb2a2e3144c47905638e3b83a78d3a
Ana Slipcevic <slipcevic.ana@gmail.com>
2017-05-18 03:43:22 +0100

    Syntax

--- src/client/app/markets/markets-routing.module.ts
+++ src/client/app/markets/markets-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule }         from '@angular/router';
-import { MarketsComponent }     from './markets.component';
+import { NgModule }                     from '@angular/core';
+import { RouterModule }                 from '@angular/router';
+import { MarketsComponent }             from './markets.component';
 
 @NgModule({
   imports: [
