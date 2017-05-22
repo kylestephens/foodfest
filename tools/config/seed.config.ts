405b7bc67941b890ab73489015a62b0da1046684
Kyle Stephens <kylestephens@live.ie>
2017-05-22 03:16:17 +0100

    Tidy up

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -144,7 +144,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular-seed!';
+  APP_TITLE = 'Welcome to FoodFest | The better way to find event catering';
 
   /**
    * Tracking ID.
